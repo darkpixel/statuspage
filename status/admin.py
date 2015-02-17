54e2a18af5b74308518f1f69258ada1a50d03d03
Aaron C. de Bruyn <aaron@heyaaron.com>
2015-02-16 17:04:39 -0800

    Switch to new register method

--- status/admin.py
+++ status/admin.py
@@ -12,20 +12,18 @@ class BaseAdmin(admin.ModelAdmin):
     save_on_top = True
 
 
+@admin.register(Status)
 class StatusAdmin(BaseAdmin):
     list_display = ['pk', 'name', 'type', 'icon']
 
 
+@admin.register(Incident)
 class IncidentAdmin(BaseAdmin):
     list_display = ['pk', 'created', 'updated', 'user', 'name']
     list_filter = ['user']
 
 
+@admin.register(IncidentUpdate)
 class IncidentUpdateAdmin(BaseAdmin):
     list_display = ['pk', 'created', 'updated', 'incident', 'user', 'status', 'description']
     list_filter = ['user', 'status']
-
-
-admin.site.register(Status, StatusAdmin)
-admin.site.register(Incident, IncidentAdmin)
-admin.site.register(IncidentUpdate, IncidentUpdateAdmin)
