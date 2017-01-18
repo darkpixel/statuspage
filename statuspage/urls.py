67a8bc3f069ca4ea145f83eb46a4a8431cec5888
Aaron C. de Bruyn <aaron@heyaaron.com>
2017-01-17 20:53:29 -0800

    Fix login views

--- statuspage/urls.py
+++ statuspage/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView, ListView, DetailView
 
@@ -9,8 +10,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^', include('status.urls', namespace='status', app_name='status')),
-#    url(r'^account/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}, 'login'),
-#    url(r'^account/logout/$', 'django.contrib.auth.views.logout', 'logout'),
+    url(r'^account/login/$', login, {'template_name': 'admin/login.html'}, 'login'),
+    url(r'^account/logout/$', logout, 'logout'),
     url(r'^avatar/', include('avatar.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
