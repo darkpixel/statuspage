f205232756743f2e1c904494a6c8d9c7a6bc1f8c
Aaron C. de Bruyn <aaron@heyaaron.com>
2017-07-04 14:16:26 -0700

    Revert "Switch to render"
    This reverts commit c3c86c14f26393da178b91d0d8f892dd7d44e1b7.

--- statuspage/settings.py
+++ statuspage/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'status.context_processors.analytics',
             ],
